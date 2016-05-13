731f3c99737b2aa27bea3eeeef75778b8d2e4dd0
tfarrell-vz <farrell@abiresearch.com>
2016-05-13 16:02:14 -0400

    Change Nil to cNil so that it doesn't conflict with stdlib

--- src/main/scala/com/datastructures/Listf.scala
+++ src/main/scala/com/datastructures/Listf.scala
@@ -2,5 +2,5 @@ package com.datastructures
 
 
 sealed trait cList[+A]
-case object Nil extends cList[Nothing]
+case object cNil extends cList[Nothing]
 case class Cons[A](head: A, tail: cList[A]) extends cList[A]
