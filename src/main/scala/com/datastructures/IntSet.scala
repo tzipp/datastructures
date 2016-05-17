84f8d108bc0376bf626e4489c48e602b16f92f1e
tfarrell-vz <farrell@abiresearch.com>
2016-05-17 16:04:13 -0400

    Exploring filter with IntSets

--- src/main/scala/com/datastructures/IntSet.scala
+++ src/main/scala/com/datastructures/IntSet.scala
@@ -5,6 +5,8 @@ abstract class IntSet {
   def insert(x: Int): IntSet
   def contains(x: Int): Boolean
   def union(other: IntSet): IntSet
+  def filter(p: Int => Boolean): IntSet
+  def filterAcc(p: Int => Boolean, acc: IntSet): IntSet
   def traverse(): String
 }
 
@@ -12,6 +14,8 @@ object Empty extends IntSet {
   def insert(x: Int): IntSet = new NonEmpty(x, Empty, Empty)
   def contains(x: Int): Boolean = false
   def union(other: IntSet): IntSet = other
+  def filter(p: Int => Boolean): IntSet = this
+  def filterAcc(p: Int => Boolean, acc: IntSet) = acc
   def traverse(): String = ""
 }
 
@@ -30,6 +34,17 @@ class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
   def union(other: IntSet): IntSet =
     ((left union right) union other) insert elem
 
+  def filter(p: Int => Boolean): IntSet = filterAcc(p, Empty)
+
+  def filterAcc(p: Int => Boolean, acc: IntSet): IntSet = {
+    val nextAcc = {
+      if (p(elem)) acc.insert(elem)
+      else acc
+    }
+
+    right.filterAcc(p, left.filterAcc(p, nextAcc))
+  }
+
   def traverse(): String =
     elem.toString + left.traverse() + right.traverse()
 }
@@ -39,8 +54,11 @@ object IntSetTest {
     val set1 = new NonEmpty(5, Empty, Empty)
     val set2 = set1.insert(2).insert(3).insert(7)
     println(set2.traverse())
+    // println(set2.traverse())
+    val set3 = set2.filter((x: Int) => x > 3)
+    println(set3.traverse())
 
-    val set3 = new NonEmpty(1, Empty, Empty)
-    println((set2 union set3).traverse())
+    // val set3 = new NonEmpty(1, Empty, Empty)
+    // println((set2 union set3).traverse())
   }
 }
