1e3724fde40d8c8e8314d1c5846021a466580092
tfarrell-vz <farrell@abiresearch.com>
2016-05-17 10:44:15 -0400

    Exploring IntSet more

--- src/main/scala/com/datastructures/IntSet.scala
+++ src/main/scala/com/datastructures/IntSet.scala
@@ -5,12 +5,14 @@ abstract class IntSet {
   def insert(x: Int): IntSet
   def contains(x: Int): Boolean
   def union(other: IntSet): IntSet
+  def traverse(): String
 }
 
 object Empty extends IntSet {
   def insert(x: Int): IntSet = new NonEmpty(x, Empty, Empty)
   def contains(x: Int): Boolean = false
   def union(other: IntSet): IntSet = other
+  def traverse(): String = ""
 }
 
 class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
@@ -27,4 +29,18 @@ class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
 
   def union(other: IntSet): IntSet =
     ((left union right) union other) insert elem
+
+  def traverse(): String =
+    elem.toString + left.traverse() + right.traverse()
+}
+
+object IntSetTest {
+  def run():Unit = {
+    val set1 = new NonEmpty(5, Empty, Empty)
+    val set2 = set1.insert(2).insert(3).insert(7)
+    println(set2.traverse())
+
+    val set3 = new NonEmpty(1, Empty, Empty)
+    println((set2 union set3).traverse())
+  }
 }
